921d910aa6c2575429087d885464dd495cc62743
Tyler Beck <softwaretpd2035@gmail.com>
2022-10-31 08:47:03 -0400

    updated local auth

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/auth')
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+
+
 router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
@@ -12,3 +14,4 @@ router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
 
 module.exports = router
+
